016d1281250dfc4184e15befad96811e30ad581e
danielthane <dthane8294@hotmail.co.uk>
2023-07-27 12:21:02 +0100

    Add resize and reset functionality

--- etch.js
+++ etch.js
@@ -1,24 +1,50 @@
 const container = document.querySelector('.container');
-const width = 16;
-const cellWidth = (500/width).toString();
+const resizeBtn = document.querySelector('.size-btn');
+const resetBtn = document.querySelector('.reset-btn');
 
-for(let i = 0; i < width; i++){
-    for (let j = 0; j < width; j++){
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        cell.style.width = `${cellWidth}px`;
-        cell.style.height = `${cellWidth}px`;
-        container.appendChild(cell);
+let width = 16;
+let cells = document.querySelectorAll('.cell');
+
+function createPad(container, width){
+    let cellWidth = (500/width).toString();
+    // Loops for each row and column
+    for(let i = 0; i < width; i++){
+        for (let j = 0; j < width; j++){
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.style.width = `${cellWidth}px`;
+            cell.style.height = `${cellWidth}px`;
+            container.appendChild(cell);
+        }
     }
+    // Sets up a grid layout
+    container.style.gridTemplateColumns = `repeat(${width}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${width}, 1fr)`
+    cells = document.querySelectorAll('.cell');
 }
 
-container.style.display = "grid";
-container.style.gridTemplateColumns = `repeat(${width}, 1fr)`
-container.style.gridTemplateRows = `repeat(${width}, 1fr)`
-
-const cells = document.querySelectorAll('.cell');
-cells.forEach(cell => {
-    cell.addEventListener('mouseover', (e) =>{
-        cell.style.backgroundColor = "black";
+// Adds hover functionality
+function initialiseCells(){
+    cells.forEach(cell => {
+        cell.addEventListener('mouseover', (e) =>{
+            cell.style.backgroundColor = "black";
+        })
     })
+}
+
+// Creates initial board
+createPad(container, width);
+initialiseCells();
+
+
+// Adds functionality to resize the board
+resizeBtn.addEventListener('click', (e)=>{
+    width = Number(prompt("Please enter a number 100 or less"));
+    cells.forEach(cell => cell.remove())
+    createPad(container, width);
+    initialiseCells();
+})
+
+resetBtn.addEventListener('click', (e) => {
+    cells.forEach(cell => cell.style.backgroundColor = "aliceblue");
 })
